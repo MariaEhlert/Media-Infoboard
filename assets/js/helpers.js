c81626a7a6df6cc57211bc8abdd867b54bec6092
TordisLaksa <88707360+TordisLaksa@users.noreply.github.com>
2021-12-01 09:40:29 +0100

    Activity upload

--- assets/js/helpers.js
+++ assets/js/helpers.js
@@ -4,27 +4,113 @@ export const myFetch = async (url, options = null) => {
     let response;
 
     //forsøger at lave et kald
-    try{
+    try {
         //hvis options er null skal den kun bruge url
-        if(!options){
-            response = await fetch (url);
-        //ellers skal den bruge både url og options
-        }else{
-            response = await fetch (url, options);
+        if (!options) {
+            response = await fetch(url);
+            //ellers skal den bruge både url og options
+        } else {
+            response = await fetch(url, options);
         }
         //laver en parse på response 
         const result = await response.json();
         //gemmer response info i object key response
         result.response = {
-            ok : response.ok,
-            status : response.status,
-            statusText : response.statusText
+            ok: response.ok,
+            status: response.status,
+            statusText: response.statusText
         }
         //retuner result
         return result;
     }
     //fanger evt fejl i fetch
-    catch (err){
+    catch (err) {
         console.error(`Der er fejl i myFetch: ${err}`);
     }
 }
+
+//Funktion til at hente dk dag
+export const day2local = intDay => {
+    let day;
+    switch (intDay) {
+        default:
+            break;
+        case 0:
+            day = 'Søndag';
+            break;
+        case 1:
+            day = 'Mandag';
+            break;
+        case 2:
+            day = 'Tirsdag';
+            break;
+        case 3:
+            day = 'Onsdag';
+            break;
+        case 4:
+            day = 'Torsdag';
+            break;
+        case 5:
+            day = 'Fredag';
+            break;
+        case 6:
+            day = 'Lørdag';
+            break;
+    }
+
+    return day;
+}
+
+
+//Funktion til at hente dk måned
+export const month2local = intMonth => {
+    let month;
+    switch (intMonth) {
+        default:
+            break;
+        case 0:
+            month = 'Januar';
+            break;
+        case 1:
+            month = 'Febuar';
+            break;
+        case 2:
+            month = 'Marts';
+            break;
+        case 3:
+            month = 'April';
+            break;
+        case 4:
+            month = 'Maj';
+            break;
+        case 5:
+            month = 'Juni';
+            break;
+        case 6:
+            month = 'Juli';
+            break;
+        case 7:
+            month = 'August';
+            break;
+        case 8:
+            month = 'September';
+            break;
+        case 9:
+            month = 'Oktober';
+            break;
+        case 10:
+            month = 'November';
+            break;
+        case 11:
+            month = 'December';
+            break;
+    }
+
+    return month;
+}
+
+export const dayDateMonth2local = strDate => {
+    const date = new Date(strDate);
+    return `${day2local(date.getDay())} d. ${date.getDate()}. ${month2local(date.getMonth())}`;
+}
+
---
+++ assets/js/main.js
@@ -0,0 +1,3 @@
+import { getActivityData } from "./componenter/activityData.js";
+
+getActivityData();
