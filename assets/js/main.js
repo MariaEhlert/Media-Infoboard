cdd3a890cd69b98ff5387a1089b0bcd268f6047b
Gawmane <gawman.e@hotmail.com>
2021-12-06 09:12:25 +0100

    samlet kode

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,8 @@
-<<<<<<< HEAD
-=======
 //Impotere vores aktiviteter
 import { getActivityData } from "./componenter/activityData.js";
 //Kalder vores aktivitets function
 getActivityData();
 
->>>>>>> 1b195d2e7ff3481d25b47e2da06f8c6b1ad7dbe0
 //Impotere vores menu 
 import { dishFunction } from "./componenter/dish.js";
 //Kalder menu funktion 
@@ -20,8 +17,4 @@ import { weatherFunction } from "./componenter/weather.js";
 weatherFunction()
 
 import { busFunction } from "./componenter/bustime.js";
-<<<<<<< HEAD
 busFunction()
-=======
-busFunction()
->>>>>>> 1b195d2e7ff3481d25b47e2da06f8c6b1ad7dbe0
